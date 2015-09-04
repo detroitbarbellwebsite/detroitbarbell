fc0144e0e37c1bdd1704369d1bf6e945c10caf30
Drew Young <detroitbarbellwebsite@gmail.com>
2015-09-03 23:11:26 -0400

    Initial Check-In for DBB website

---
+++ DetroitBarbellWebsite/Scripts/scrolling.js
@@ -0,0 +1,26 @@
+﻿$(function () {
+    //fills image for background
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
+    });
+
+    //sets offset for navbar when scrolling
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 160
+    });
+
+    //scrolling animation
+    $('nav a, .down-button a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: ($($(this).attr('href')).offset().top - 100)
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    //implement parallax scrolling
+    $(window).stellar();
+
+    //initialize WOW for element animation
+    new WOW().init(); 
+});
