ffb86de05c2552747ca6b77f5bfa87555b4bc81c
Drew Young <detroitbarbellwebsite@gmail.com>
2015-09-05 11:46:11 -0400

    Initial check-In for AngularJS Integration

---
+++ DetroitBarbellWebsite/app/app.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    var app = angular.module('app', ['ngRoute']);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "./app/partials/home.html"
+            })
+            .otherwise({ rediectTo: "/" });
+    });
+}());
---
+++ DetroitBarbellWebsite/app/controllers/DetroitMainController.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    var app = angular.module("app");
+    var detroitMainCtrl = function ($scope, dbbDataService, $routeParams) {
+
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the data";
+        };
+
+        var onUserComplete = function (data) {
+            $scope.homePage = data;
+        }
+
+        var setHomePageData = function (data) {
+            $scope.homePage = data;
+        }
+
+        //var promise = $http.get('https://api.github.com/users/angular');
+        //promise.then(onUserComplete);
+
+        //$http.get('https://api.github.com/users/angular')
+        //    .then(onUserComplete, onError);
+
+        // initialized variables
+        //var homePageData = dbbDataService.getHomePageData();
+        //var homePageData = dbbDataService.getSingleAboutMe(); 
+        dbbDataService.getHomePageDataApi().then(onUserComplete, onError);
+        //setHomePageData(homePageData);
+    };
+
+    app.controller('DetroitMainController', detroitMainCtrl);
+}());
