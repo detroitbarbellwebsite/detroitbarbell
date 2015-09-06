3ad02ec5639ce61d0ab81ac75986b9ddaf6b1399
Drew Young <detroitbarbellwebsite@gmail.com>
2015-09-06 00:17:39 -0400

    DataTables Font Addition Service Addition for Record Board Data

--- DetroitBarbellWebsite/app/controllers/RecordBoardController.js
+++ DetroitBarbellWebsite/app/controllers/RecordBoardController.js
@@ -1,9 +1,18 @@
 ﻿(function () {
     var app = angular.module("app");
-    var recordBoardCtrl = function ($scope) {
-        $scope.initDataTable = function () {
+    var recordBoardCtrl = function ($scope, recordBoardService, $timeout) {
+
+        var getSquatRecords = recordBoardService.getRecordBoardData();
+
+        $scope.squatList = getSquatRecords; 
+
+        $timeout(function () {
             $('#recordboard').dataTable();
-        };
+        }, 0)
+
+        //$scope.initDataTable = function () {
+        //    $('#recordboard').dataTable();
+        //};
     };
 
     app.controller('RecordBoardController', recordBoardCtrl);
---
+++ DetroitBarbellWebsite/app/services/recordBoardService.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    var recordBoardService = function ($http) {
+        var getRecordBoardData = function () {
+            var json = [{ "Lifter": "Lifter ", "Gender": "Male", "WeightClass": "132", "Record": 500, "RawOrEquipped": "Raw" },
+{ "Lifter": "LifterLongName OneLongerLastName", "Gender": "Female", "WeightClass": "132", "Record": 500, "RawOrEquipped": "Equipped" },
+{ "Lifter": "Lifter ", "Gender": "Male", "WeightClass": "148", "Record": 600, "RawOrEquipped": "Raw" },
+{ "Lifter": "LifterLongName OneLongerLastName", "Gender": "Female", "WeightClass": "148", "Record": 600, "RawOrEquipped": "Equipped" },
+{ "Lifter": "Lifter ", "Gender": "Male", "WeightClass": "165", "Record": 700, "RawOrEquipped": "Raw" },
+{ "Lifter": "LifterLongName OneLongerLastName", "Gender": "Female", "WeightClass": "165", "Record": 700, "RawOrEquipped": "Equipped" },
+{ "Lifter": "Lifter ", "Gender": "Male", "WeightClass": "181", "Record": 800, "RawOrEquipped": "Equipped" },
+{ "Lifter": "LifterLongName OneLongerLastName", "Gender": "Female", "WeightClass": "181", "Record": 800, "RawOrEquipped": "Equipped" },
+{ "Lifter": "Lifter ", "Gender": "Male", "WeightClass": "198", "Record": 800, "RawOrEquipped": "Equipped" },
+{ "Lifter": "LifterLongName OneLongerLastName", "Gender": "Female", "WeightClass": "SHW", "Record": 900, "RawOrEquipped": "Equipped" },
+{ "Lifter": "Lifter ", "Gender": "Male", "WeightClass": "220", "Record": 1000, "RawOrEquipped": "Equipped" },
+{ "Lifter": "Lifter ", "Gender": "Male", "WeightClass": "242", "Record": 1000, "RawOrEquipped": "Equipped" },
+{ "Lifter": "Lifter ", "Gender": "Male", "WeightClass": "275", "Record": 1000, "RawOrEquipped": "Equipped" },
+{ "Lifter": "Lifter ", "Gender": "Male", "WeightClass": "308", "Record": 1000, "RawOrEquipped": "Equipped" },
+{ "Lifter": "Lifter ", "Gender": "Male", "WeightClass": "SHW", "Record": 1000, "RawOrEquipped": "Equipped" }]
+
+            return json;
+        };
+
+        return {
+            getRecordBoardData: getRecordBoardData,
+        };
+    };
+
+    var module = angular.module("app");
+    // register a service with angular
+    module.factory("recordBoardService", recordBoardService);
+}());
