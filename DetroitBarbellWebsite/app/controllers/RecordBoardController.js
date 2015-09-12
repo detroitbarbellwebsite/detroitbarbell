5dab7f59d4e97f8cce4a01223cd8823e49254269
Drew Young <detroitbarbellwebsite@gmail.com>
2015-09-12 12:54:24 -0400

    -Re-did Record-Board to show Overall Records for Men/Women -Removed title from Contact -Commented out logic behind retreiving data for record boards.

--- DetroitBarbellWebsite/app/controllers/RecordBoardController.js
+++ DetroitBarbellWebsite/app/controllers/RecordBoardController.js
@@ -2,42 +2,49 @@
     var app = angular.module("app");
     var recordBoardCtrl = function ($scope, recordBoardService, $timeout) {
 
-        var getSquatRecords = recordBoardService.getSquatRecordData();
-        var getBenchRecords = recordBoardService.getBenchRecordData();
-        var getDeadliftRecords = recordBoardService.getDeadliftRecordData();
-        var getTotalRecords = recordBoardService.getTotalRecordData(); 
+        //var getSquatRecords = recordBoardService.getSquatRecordData();
+        //var getBenchRecords = recordBoardService.getBenchRecordData();
+        //var getDeadliftRecords = recordBoardService.getDeadliftRecordData();
+        //var getTotalRecords = recordBoardService.getTotalRecordData(); 
 
-        $scope.squatList = getSquatRecords;
-        $scope.benchList = getBenchRecords;
-        $scope.deadliftList = getDeadliftRecords;
-        $scope.totalList = getTotalRecords; 
+        //$scope.squatList = getSquatRecords;
+        //$scope.benchList = getBenchRecords;
+        //$scope.deadliftList = getDeadliftRecords;
+        //$scope.totalList = getTotalRecords; 
 
-        $timeout(function () {
-            $('#SquatRecords').dataTable({
-                "aaSorting": [[2, "asc"], [1, "desc"]],
-                "paging": false
-            });
+        //$timeout(function () {
+        //    $('#SquatRecords').dataTable({
+        //        "aaSorting": [[2, "asc"], [1, "desc"]],
+        //        "paging": false
+        //    });
 
-            $('#BenchRecords').dataTable({
-                "order": [[2, "asc"]],
-                "paging": false
-            });
+        //    $('#BenchRecords').dataTable({
+        //        "order": [[2, "asc"]],
+        //        "paging": false
+        //    });
 
 
-            $('#DeadliftRecords').dataTable({
-                "order": [[2, "asc"]],
-                "paging": false
-            });
+        //    $('#DeadliftRecords').dataTable({
+        //        "order": [[2, "asc"]],
+        //        "paging": false
+        //    });
 
-            $('#TotalRecords').dataTable({
-                "order": [[2, "asc"]],
-                "paging": false
-            });
-        }, 0)
+        //    $('#TotalRecords').dataTable({
+        //        "order": [[2, "asc"]],
+        //        "paging": false
+        //    });
+        //}, 0)
 
-        //$scope.initDataTable = function () {
-        //    $('#recordboard').dataTable();
-        //};
+        $('#OverallMens').dataTable({
+            "bSort": false,
+            "paging": false,
+            "sDom": '<"clear">'
+        });
+        $('#OverallWomens').dataTable({
+            "bSort": false,
+            "paging": false,
+            "sDom": '<"clear">'
+        });
     };
 
     app.controller('RecordBoardController', recordBoardCtrl);
